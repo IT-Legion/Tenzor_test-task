8cadd261cb4d3187a2cf060d3221fd87f87ce5bf
Сергей Перевозников <parik073@gmail.com>
2023-09-06 20:15:45 +0200

    Добавлены  не которые тесты

--- task_1.py
+++ task_1.py
@@ -3,32 +3,43 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from  brower import  ceheckbox
-import time
+import  os
+
 
 def get_url(driver):
+    '''
+     Производит поиск и открывает 1 страницу
+    :return: Словарь для теста
+    '''
+    parameters = dict()
     search_string = driver.find_element(By.XPATH, '//*[@id="text"]')
+    #Отправить в тест
+    parameters['search_string'] = search_string.is_displayed()
     search_string.send_keys('Тензор')
+    # Найти таблицу
+
     search_string.submit()
     search_results = driver.find_elements(By.CSS_SELECTOR, ".organic__url-text")
     search_results[0].click()
     driver.switch_to.window(driver.window_handles[1])
-    url = driver.current_url
-    return url
-
+    parameters['url_task1'] = driver.current_url
+    driver.close()
+    return parameters
 
 # инициализация
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 url = 'https://ya.ru'
 
-
 #открывает сайт
 driver.get(url)
 if driver.title == 'Ой!':
     ceheckbox(driver)
 
-first_link = get_url(driver)
-print(first_link)
+
+parameters = get_url(driver)
+for i,j in parameters.items():
+    print(i,j)
 
 
 
--- task_2.py
+++ task_2.py
@@ -7,31 +7,36 @@ from  brower import  ceheckbox
 import time
 
 def go_to_images(driver):
+    parameters = dict()
     element = driver.find_element(By.XPATH, '//*[@id="text"]')
     action.move_to_element(element).click(element).perform()
     time.sleep(1)
     element = driver.find_element(By.LINK_TEXT, 'Все')
+    parameters['button_all'] = element.is_displayed()
     action.move_to_element(element).click(element).perform()
     time.sleep(1)
     element = driver.find_element(By.LINK_TEXT, 'Картинки')
     action.move_to_element(element).click(element).perform()
     time.sleep(1)
     driver.switch_to.window(driver.window_handles[1])
+    parameters['url_task2'] = driver.current_url
     time.sleep(1)
     first_link = driver.find_elements(By.CLASS_NAME, 'PopularRequestList-SearchText')
     first_link[0].click()
     time.sleep(1)
     element = driver.find_element(By.XPATH, '//a[contains(@class,"serp-item__link")]')
     element.click()
+    parameters['url_image_1'] = driver.current_url
     time.sleep(1)
     element = driver.find_element(By.XPATH, '//div[contains(@class,"CircleButton_type_next")]')
     element.click()
-    print(element)
-
-    time.sleep(100)
-
-
-
+    time.sleep(1)
+    parameters['url_image_2'] = driver.current_url
+    element = driver.find_element(By.XPATH, '//div[contains(@class,"CircleButton_type_prev")]')
+    element.click()
+    time.sleep(1)
+    parameters['url_image_test'] = driver.current_url
+    return parameters
 
 
 
@@ -47,8 +52,10 @@ url = 'https://ya.ru/'
 driver.get(url)
 if driver.title == 'Ой!':
     ceheckbox(driver)
-go_to_images(driver)
+parameters = go_to_images(driver)
 
+for i,j in parameters.items():
+    print(i,j)
 
 time.sleep(15)
 
